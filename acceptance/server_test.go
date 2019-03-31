2d92349569c42e9a253ee812d1f17aa8f43216ec
Josh Hill <jamesjoshuahill@gmail.com>
2019-03-31 15:10:35 +0000

    Create cipher endpoint responds with JSON

--- acceptance/server_test.go
+++ acceptance/server_test.go
@@ -1,6 +1,7 @@
 package acceptance_test
 
 import (
+	"io/ioutil"
 	"net/http"
 	"strings"
 
@@ -9,13 +10,32 @@ import (
 )
 
 var _ = Describe("Server", func() {
-	It("accepts an encryption request", func() {
+	It("accepts a valid create cipher request", func() {
 		res, err := http.Post(serverUrl("v1/ciphers"), "application/json", strings.NewReader(`{
 			"data": "some plain text",
 			"resource_id": "client cipher id"
 		}`))
 		Expect(err).NotTo(HaveOccurred())
-
 		Expect(res.StatusCode).To(Equal(http.StatusOK))
+		Expect(res.Header.Get("Content-Type")).To(Equal("application/json"))
+
+		body, err := ioutil.ReadAll(res.Body)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(body).To(MatchJSON(`{
+			"id": "server cipher id",
+			"data": "some plain text",
+			"resource_id": "client cipher id"
+		}`))
+	})
+
+	It("rejects a malformed create cipher request", func() {
+		res, err := http.Post(serverUrl("v1/ciphers"), "application/json", strings.NewReader("not json"))
+		Expect(err).NotTo(HaveOccurred())
+		Expect(res.StatusCode).To(Equal(http.StatusBadRequest))
+		Expect(res.Header.Get("Content-Type")).To(Equal("application/json"))
+
+		body, err := ioutil.ReadAll(res.Body)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(string(body)).To(ContainSubstring("error decoding request body"))
 	})
 })
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -20,6 +21,55 @@ func main() {
 	log.Fatalln(err)
 }
 
-func createCipherHandler(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusOK)
+type createCipherRequest struct {
+	Data       string `json:"data"`
+	ResourceID string `json:"resource_id"`
+}
+
+type createCipherResponse struct {
+	ID string `json:"id"`
+	createCipherRequest
+}
+
+type errorResponse struct {
+	Message string `json:"error"`
+}
+
+func createCipherHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	body := &createCipherRequest{}
+	err := json.NewDecoder(r.Body).Decode(body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, errorResponseBody("error decoding request body", err))
+		return
+	}
+
+	cipher := createCipherResponse{
+		ID:                  "server cipher id",
+		createCipherRequest: *body,
+	}
+
+	resBody, err := json.Marshal(cipher)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, errorResponseBody("error encoding response body", err))
+		return
+	}
+
+	w.Write(resBody)
+}
+
+func errorResponseBody(context string, err error) string {
+	errRes := errorResponse{
+		Message: fmt.Sprintf("%s: %s", context, err),
+	}
+
+	resBody, err := json.Marshal(errRes)
+	if err != nil {
+		return fmt.Sprintf(`{"error":"error encoding error response body: %s"}`, err)
+	}
+
+	return string(resBody)
 }
